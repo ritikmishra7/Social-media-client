a551f8610ab4d0ef79efca515382b5a873cde16a
Ritik Mishra <ritikmishra7@gmail.com>
2023-03-16 22:07:08 +0530

    Refresh Token bug fixed

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -44,9 +44,10 @@ axiosClient.interceptors.response.use(
         const statusCode = data.statusCode;
         const error = data.error;
 
-        store.dispatch(settoastData({ type: 'error', message: error }));
+        if (error !== 'Invalid access key')
+            store.dispatch(settoastData({ type: 'error', message: error }));
 
-        if (statusCode === 401 && originalRequest.url === `${process.env.REACT_APP_SERVER_BASE_URL}/auth/refresh`) //means the refresh token has expired
+        if (statusCode === 401 && originalRequest.url === '/auth/refresh') //means the refresh token has expired
         {
             removeItem(ACCESS_TOKEN_KEY);
             window.location.replace('/auth/login', '_self');
@@ -54,19 +55,21 @@ axiosClient.interceptors.response.use(
         }
 
         if (statusCode === 401) {    //get new accessToken from refresh api as access token has expired
-            const response = await axios.create({
+            const responseFromrefresh = await axios.create({
                 withCredentials: true
-            }).get(`${process.env.REACT_APP_SERVER_BASE_URL}/auth/refresh`);
-            if (response.status === 'ok') {
-                setItem(ACCESS_TOKEN_KEY, response.result.accessToken);
-                originalRequest.headers['Authorization'] = `Bearer ${response.result.accessToken}`;
-                return axios(originalRequest);
+            }).get(`${baseURL}auth/refresh`);
+
+            if (responseFromrefresh.data.status === 'ok') {
+                setItem(ACCESS_TOKEN_KEY, responseFromrefresh.data.result.accessToken);
+                originalRequest.headers['Authorization'] = `Bearer ${responseFromrefresh.data.result.accessToken}`;
+                return axiosClient(originalRequest);
             }
             else {
                 removeItem(ACCESS_TOKEN_KEY);
                 window.location.replace('/auth/login', '_self');
                 return Promise.reject(error);
             }
+
         }
 
         return Promise.reject(error);
